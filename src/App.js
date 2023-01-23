81d8486dbe6e74e0d95ed99e14edfceb87d12ccc
paha <pahany4@mail.ru>
2023-01-23 20:51:53 +0700

    vite react supabase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import './App.css';
+import { createClient } from '@supabase/supabase-js'
 
 function App() {
+
+  const options = {
+    // дефолтная схема
+    schema: 'public',
+    headers: { 'x-my-custom-header': 'my-app-name' },
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true
+  }
+
+  //const supabase = createClient(url, key, options)
+  //const supabase = createClient('https://my-app.supabase.co', 'public-anon-key', options)
+
+  const supabase = createClient('https://my-app.supabase.co', 'public-anon-key', {
+    fetch: fetch.bind(globalThis)
+  })
+
   return (
     <div className="App">
 
